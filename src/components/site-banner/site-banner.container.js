8bd465353da43a2810917890663bb12908fdb5f1
DanielJDufour <daniel.j.dufour@gmail.com>
2018-10-12 02:45:00 +0200

    added site-banner

---
+++ src/components/site-banner/site-banner.container.js
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux'
+import SiteBannerComponent from './site-banner.component'
+import { getConfigValue } from 'utils'
+
+const mapStateToProps = ({ siteConfig }) => {
+  return {
+    backgroundImage: getConfigValue(siteConfig, 'images.background')
+  }
+}
+
+export default connect(mapStateToProps)(SiteBannerComponent)
