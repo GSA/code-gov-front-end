f38b0df616a7930c6f4ccd1dec34ba2366973ec1
DanielJDufour <daniel.j.dufour@gmail.com>
2018-10-18 21:17:34 +0000

    updated browse results

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -6,11 +6,13 @@ import siteConfig from './site-config'
 import query from './query'
 import searchHistory from './search'
 import browseFilters from './browse-filters'
+import browseResults from './browse-results'
 import searchFilters from './search-filters'
 
 const rootReducer = combineReducers({
   agencies,
   browseFilters,
+  browseResults,
   filters,
   query,
   repo,
