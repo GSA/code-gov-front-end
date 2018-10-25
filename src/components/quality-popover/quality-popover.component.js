a68a7733aac42f410a4d8d9018073fdfd03476d1
DanielJDufour <daniel.j.dufour@gmail.com>
2018-10-25 03:56:02 +0000

    fixed typos in quality-popover

--- src/components/quality-popover/quality-popover.component.js
+++ src/components/quality-popover/quality-popover.component.js
@@ -5,12 +5,12 @@ export default class QualityPopover extends Component {
 
   render() {
     return (
-      <div style="position: absolute; right: 0; top: -25px;">
-        <span class="data-quality-title">Data Quality Score</span>
-        <div class="icon icon-help-circled popper">
-          <div class="popover desktop left">The Data Quality Score is determined by using the information provided by Agencies in their <Link to="/policy-guide/docs/compliance/inventory-code">code.json</Link> and by factors such as completeness and adherence to the <Link to="/policy-guide/docs/compliance/inventory-code">metadata schema</Link>.</div>
-          <div class="popover mobile left">
-            <div class="popover-title">Data Quality Score</div>
+      <div style={{position: 'absolute', right: '0', top: '-25px'}}>
+        <span className="data-quality-title">Data Quality Score</span>
+        <div className="icon icon-help-circled popper">
+          <div className="popover desktop left">The Data Quality Score is determined by using the information provided by Agencies in their <Link to="/policy-guide/docs/compliance/inventory-code">code.json</Link> and by factors such as completeness and adherence to the <Link to="/policy-guide/docs/compliance/inventory-code">metadata schema</Link>.</div>
+          <div className="popover mobile left">
+            <div className="popover-title">Data Quality Score</div>
             The Data Quality Score is determined by using the information provided by Agencies in their <Link to="/policy-guide/docs/compliance/inventory-code">code.json</Link> and by factors such as completeness and adherence to the <Link to="/policy-guide/docs/compliance/inventory-code">metadata schema</Link>.</div>
         </div>
       </div>
