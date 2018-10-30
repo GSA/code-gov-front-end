b4c7cd230c72189f2ba149b5ea1a006113c8224b
DanielJDufour <daniel.j.dufour@gmail.com>
2018-10-30 20:03:06 +0000

    fixed issue 5

--- src/components/footer/footer.container.js
+++ src/components/footer/footer.container.js
@@ -1,9 +1,12 @@
 import { connect } from 'react-redux';
+import get from 'lodash.get'
 import { getConfigValue } from 'utils/other'
 import Footer from './footer.component'
 
-const mapStateToProps = ({ siteConfig }) => {
+const mapStateToProps = ({ router, siteConfig }) => {
+  const onHomePage = get(router, 'location.pathname') === '/'
   return {
+    color: onHomePage ? 'dark' : 'white',
     links: getConfigValue(siteConfig, 'content.footer.links'),
     logos: getConfigValue(siteConfig, 'content.footer.logos')
   }
