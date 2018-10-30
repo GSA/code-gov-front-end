ce0833cde6c285d90603a35e52896d487435d8d9
DanielJDufour <daniel.j.dufour@gmail.com>
2018-10-30 20:48:50 +0000

    updated site config and fixed bug w external links

--- src/components/menu/subcomponents/secondary-dropdown.js
+++ src/components/menu/subcomponents/secondary-dropdown.js
@@ -1,17 +1,22 @@
 import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
-import { some } from '@code.gov/cautious'
+import { some, startsWith } from '@code.gov/cautious'
+
+function LinkPart(name, url) {
+  if (startsWith(url, 'https')) {
+    return <a key={url} href={url} target='_blank'>{name}</a>
+  } else {
+    return <Link key={url} to={url}>{name}</Link>
+  }
+}
 
 export default function SecondaryDropdown({ menuOption, onClick }) {
   if (some(menuOption.links)) {
     return (
       <ul role="menu">
-        {menuOption.links.map(link => {
-          return (
-            <li key={link.url} role="none">
-              <Link to={link.url}>{link.name}</Link>
-            </li>
-          )
+        {menuOption.links.map(link =>{
+          const { name, url } = link
+          return LinkPart(name, url)
         })}
       </ul>
     )
