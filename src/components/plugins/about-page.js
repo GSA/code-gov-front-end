030d177538049b729693a9489a181c103c67f59e
DanielJDufour <daniel.j.dufour@gmail.com>
2018-11-02 19:13:11 +0000

    started work on about page

---
+++ src/components/plugins/about-page.js
@@ -0,0 +1,5 @@
+/* global fetch */
+import React, { Component, Fragment } from 'react'
+import { Link, NavLink, Route, Switch } from 'react-router-dom'
+import SiteBanner from 'components/site-banner'
+
