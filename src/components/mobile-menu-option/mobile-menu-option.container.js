b9335b3b80db845b735486807c468146cc015a1f
DanielJDufour <daniel.j.dufour@gmail.com>
2018-11-10 01:28:27 +0000

    dispatch collapse all mobile menu options on selection

--- src/components/mobile-menu-option/mobile-menu-option.container.js
+++ src/components/mobile-menu-option/mobile-menu-option.container.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux'
 import MobileMenuOptionComponent from './mobile-menu-option.component'
 import hideMobileMenu from 'actions/hide-mobile-menu'
 import toggleMobileMenuOption from 'actions/toggle-mobile-menu-option'
+import collapseAllMobileMenuOptions from 'actions/collapse-all-mobile-menu-options'
 
 const mapStateToProps = ({ expandedMobileMenuOptions }) => {
   return {
@@ -11,7 +12,10 @@ const mapStateToProps = ({ expandedMobileMenuOptions }) => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    hideMobileMenu: () => dispatch(hideMobileMenu()),
+    hideMobileMenu: () => {
+      dispatch(collapseAllMobileMenuOptions()),
+      dispatch(hideMobileMenu())
+    },
     toggleMobileMenuOption: name => dispatch(toggleMobileMenuOption(name))
   }
 }
