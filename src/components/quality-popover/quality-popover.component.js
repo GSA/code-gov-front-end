fc9a131abd59406eab7dddff7d4d9ec9ee0efd25
DanielJDufour <daniel.j.dufour@gmail.com>
2018-11-14 05:44:03 +0000

    updated quality popover links

--- src/components/quality-popover/quality-popover.component.js
+++ src/components/quality-popover/quality-popover.component.js
@@ -8,10 +8,10 @@ export default class QualityPopover extends Component {
       <div style={{position: 'absolute', right: '0', top: '-25px'}}>
         <span className="data-quality-title">Data Quality Score</span>
         <div className="icon icon-help-circled popper">
-          <div className="popover desktop left">The Data Quality Score is determined by using the information provided by Agencies in their <CustomLink to="/policy-guide/docs/compliance/inventory-code">code.json</CustomLink> and by factors such as completeness and adherence to the <CustomLink to="/policy-guide/docs/compliance/inventory-code">metadata schema</CustomLink>.</div>
+          <div className="popover desktop left">The Data Quality Score is determined by using the information provided by Agencies in their <CustomLink to="/about/compliance/inventory-code">code.json</CustomLink> and by factors such as completeness and adherence to the <CustomLink to="/about/compliance/inventory-code">metadata schema</CustomLink>.</div>
           <div className="popover mobile left">
             <div className="popover-title">Data Quality Score</div>
-            The Data Quality Score is determined by using the information provided by Agencies in their <CustomLink to="/policy-guide/docs/compliance/inventory-code">code.json</CustomLink> and by factors such as completeness and adherence to the <CustomLink to="/policy-guide/docs/compliance/inventory-code">metadata schema</CustomLink>.</div>
+            The Data Quality Score is determined by using the information provided by Agencies in their <CustomLink to="/about/compliance/inventory-code">code.json</CustomLink> and by factors such as completeness and adherence to the <CustomLink to="/about/compliance/inventory-code">metadata schema</CustomLink>.</div>
         </div>
       </div>
     )
