9e6c7472f851e5066492b2aed87dda2bddcda59f
DanielJDufour <daniel.j.dufour@gmail.com>
2018-11-18 22:49:19 +0000

    removed semicolons from index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/app';
-import buildStore from './build-store';
-import { Provider } from 'react-redux';
-import '../styles/_main.scss';
-import syncers from './syncers';
-import syncStore from './sync-store';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/app'
+import buildStore from './build-store'
+import { Provider } from 'react-redux'
+import '../styles/_main.scss'
+import syncers from './syncers'
+import syncStore from './sync-store'
 
-const store = buildStore();
+const store = buildStore()
 
-syncStore(store, syncers);
+syncStore(store, syncers)
 
 
 ReactDOM.render(
@@ -20,4 +20,4 @@ ReactDOM.render(
     </BrowserRouter>
   </Provider>,
   document.getElementById('app')
-);
+)
