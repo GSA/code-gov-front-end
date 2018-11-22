0dd233636fecc14f9d36fce65afbdd844ccbf95a
DanielJDufour <daniel.j.dufour@gmail.com>
2018-11-22 03:35:05 +0000

    added bundle analysis to ci

---
+++ config/webpack/webpack.analyze.js
@@ -0,0 +1,12 @@
+const merge = require('webpack-merge');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const analyzeConfig = {
+    plugins: [
+        new BundleAnalyzerPlugin({ analyzerMode: 'static' })
+    ]
+};
+
+const prodConfig = require('./webpack.prod.js');
+
+module.exports = merge(prodConfig, analyzeConfig);
