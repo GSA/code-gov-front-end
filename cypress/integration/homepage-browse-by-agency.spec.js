13afb624ede4ef9a19e48e92f927ca5924ed3a7c
hendersonanalytics <everett@hendersonanalytics.com>
2019-05-17 10:18:28 -0500

    add wait comments

--- cypress/integration/homepage-browse-by-agency.spec.js
+++ cypress/integration/homepage-browse-by-agency.spec.js
@@ -19,6 +19,11 @@ describe('homepage browse by agency', () => {
   it('only returns results for that agency', () => {
     cy.get('select')
       .select(agency)
+      // using wait not ideal: https://docs.cypress.io/guides/references/best-practices.html#Unnecessary-Waiting
+      // this will have to be refactored but was the only way I could get the test to pass
+      // aliasing the api endpoint route and passing that alias to wait looks the most promising:
+      // https://docs.cypress.io/api/commands/wait.html#Alias
+      // https://docs.cypress.io/api/commands/route.html#Examples
       .wait(3000)
       .get('.card-list-item')
       .each((item) => {
