6ece0334f3cff47d07f0aa3bbd5b2121531d930c
hendersonanalytics <everett@hendersonanalytics.com>
2019-05-21 23:21:50 -0500

    move LinkPart component to a separate file

---
+++ src/components/menu/subcomponents/link-part.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import CustomLink from 'components/custom-link'
+import { startsWith } from '@code.gov/cautious'
+
+export default function LinkPart({name, onClick, url}) {
+  if (startsWith(url, 'http') || startsWith(url, 'mailto')) {
+    return (
+      <a
+        href={url}
+        onClick={onClick}
+        data-testid="link-part-anchor"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {name}
+      </a>
+    )
+  }
+
+  return <CustomLink to={url} onClick={onClick}>{name}</CustomLink>
+}
+
+
+
