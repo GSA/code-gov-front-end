28d94e834488af3527b07707fcb9e48a85436a86
DanielJDufour <daniel.j.dufour@gmail.com>
2019-05-28 03:01:42 +0000

    added comments explaining syncers

--- src/sync-store.js
+++ src/sync-store.js
@@ -1,4 +1,28 @@
-// modified from https://github.com/reduxjs/redux/issues/303#issuecomment-125184409
+/*
+  This file allows you to add syncers (a.k.a. triggers) for your Redux store.
+  Syncers is actually an anti-pattern and should be generally discouraged.
+  However, several factors made the creation of syncers an unfortunate necessity.
+  We look forward to eventually removing this some day.
+
+  This was created to satisfy the need for caching/storing our API results
+  as well as organizing how the app updates the interface in response to user
+  selections.
+
+  syncStore takes in the store and an array of syncer objects.
+  Each syncer object has a select and sync property.
+
+  The value of select is a function that takes in a state of the store and
+  returns a value that should be watched.
+  When the value from select changes, it triggers the sync function.
+
+  The sync function is basically the function that is run when you make a change
+  that you are listening for in the Redux store. It takes
+  in the current state of the Redux store and a dispatch function which can
+  be used to dispatch actions.
+
+  modified from https://github.com/reduxjs/redux/issues/303#issuecomment-125184409
+*/
+
 function observeStore(store, select, onChange) {
   let currentState;
 
--- src/syncers.js
+++ src/syncers.js
@@ -7,9 +7,21 @@ import updateTaskResults from 'actions/update-task-results'
 import syncURLSearchParams from 'actions/sync-url-search-params'
 
 let count = 0
-let threshold = 50
+
+/*
+  This was originally put in to prevent the app from spinning out of control
+  and accidentally making hundreds of API calls.
+*/
+const threshold = 50
 
 const syncers = [
+  /*
+    This syncer updates your url when the user changes pages, searches
+    for something, or selects a filter option.
+
+    For example, this is what updates page=2 to page=3 in the url
+    when a user changes pages in /browse-projects
+  */
   {
     select: state => ([
       state.router.location.pathname,
@@ -23,6 +35,12 @@ const syncers = [
     }
   },
   {
+    /*
+      This is what listens to browse parameters like language, agency,
+      license, usage type and page number and then updates the results
+      when the user selects something in one of the filter boxes
+      or clicks to view another page of the browse results.
+    */
     select: state => state.browseParams,
     sync: (state, dispatch) => {
       console.warn("browseParams changed")
@@ -35,6 +53,10 @@ const syncers = [
     }
   },
   {
+    /*
+      This is much like the syncer above, but also listens for search terms
+      and updates the search results when the user makes a change.
+    */
     select: state => state.searchParams,
     sync: (state, dispatch) => {
       console.warn("searchParams changed")
@@ -49,6 +71,10 @@ const syncers = [
     }
   },
   {
+    /*
+      This syncer updates the results under /open-tasks when the user make a
+      selection from one of the filter boxes on that page.
+    */
     select: state => state.taskParams,
     sync: (state, dispatch) => {
       console.warn("detected change to task params")
