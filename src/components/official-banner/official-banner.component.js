b7355c013707cd1e9075fc487710c3f7e3ddd1c5
hendersonanalytics <everett@hendersonanalytics.com>
2019-06-11 19:24:02 -0500

    add data-testid attribute to component

--- src/components/official-banner/official-banner.component.js
+++ src/components/official-banner/official-banner.component.js
@@ -3,8 +3,9 @@ import React from 'react'
 export default function OfficialBanner(props) {
   const { isDark = false } = props
   const className = isDark ? 'gov-banner gov-banner__dark'  : 'gov-banner'
+  const dataTestId = isDark ? 'component-gov-banner-dark'  : 'component-gov-banner'
   return (
-    <div className={className}>
+    <div className={className} data-testid={dataTestId}>
       <gov-banner />
     </div>
   )
