c33aff12c6637a258c320a3cc0d610361ab2562d
Courtney Brousseau <courtney.brousseau@gsa.gov>
2019-07-26 14:29:39 -0400

    Formatting consistency

--- src/components/about-page/overview-introduction.component.js
+++ src/components/about-page/overview-introduction.component.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import LazyHTML from 'components/lazy-html'
 
 const dataurl = `${PUBLIC_PATH  }src/components/about-page/html/`
--- src/components/about-page/procurement.component.js
+++ src/components/about-page/procurement.component.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import LazyHTML from 'components/lazy-html'
 
-const dataurl = PUBLIC_PATH + 'src/components/about-page/html/'
+const dataurl = `${PUBLIC_PATH  }src/components/about-page/html/`
 
 const Procurement = () => <LazyHTML url={`${dataurl}compliance/how-to-procure.html`}/>
 
