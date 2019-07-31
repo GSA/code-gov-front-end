369c0a0c4ead501a304de477aac7f554be0e9b1c
Sara Cope <sara.cope@gsa.gov>
2019-07-31 14:46:43 -0400

    fix sourcemaps, add autoprefixer

--- config/webpack/webpack.shared.js
+++ config/webpack/webpack.shared.js
@@ -1,6 +1,6 @@
 const { copyFileSync, readFileSync } = require('fs')
 const { dirname, join } = require('path')
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+// const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const DefinePlugin = require('webpack/lib/DefinePlugin')
@@ -8,6 +8,7 @@ const EnvironmentPlugin = require('webpack/lib/EnvironmentPlugin')
 const EventHooksPlugin = require('event-hooks-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const sass = require('sass')
+const autoprefixer = require('autoprefixer')
 const get = require('lodash.get')
 const { map } = require('@code.gov/cautious')
 
@@ -155,12 +156,31 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          'style-loader', // creates style nodes from JS strings
-          'css-loader', // translates CSS into CommonJS
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+            options: {
+              sourceMap: true
+            }
+          },
           {
             loader: 'sass-loader', // compiles Sass to CSS
             options: {
-              implementation: sass
+              implementation: sass,
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader', // adds vendor prefixing
+            options: {
+              parser: 'postcss-scss',
+              sourceMap: true,
+              plugins: [autoprefixer]
             }
           }
         ]
@@ -209,12 +229,12 @@ module.exports = {
     new EnvironmentPlugin(['CODE_GOV_API_BASE', 'CODE_GOV_API_KEY', 'CODE_GOV_TASKS_URL']),
     new CleanWebpackPlugin([OUTPUT_PATH], { root: rootDir }),
     new CopyWebpackPlugin(patterns),
-    new FaviconsWebpackPlugin({
-      logo: './assets/img/favicon.png',
-      icons: {
-        appleStartup: false
-      }
-    }),
+    // new FaviconsWebpackPlugin({
+    //   logo: './assets/img/favicon.png',
+    //   icons: {
+    //     appleStartup: false
+    //   }
+    // }),
     new HtmlWebpackPlugin({
       hash: true,
       template: 'index.html',
