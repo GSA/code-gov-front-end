bb678406c392d1bc5c1ee0e1454e1d180cc68c1d
Sara Cope <sara.cope@gsa.gov>
2019-11-26 17:54:38 -0500

    update to uswds footer

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -6,16 +6,19 @@ import React, { PureComponent } from 'react'
 export default class Footer extends PureComponent {
   render() {
     return (
-      <footer className={this.props.color} role="contentinfo">
-        <div className="footer-content">
-          <div className="footer-part links" data-test="links">
-            <ul>
+      <footer className="usa-footer" role="contentinfo">
+        <div className="grid-container usa-footer__return-to-top">
+          <a href="#">Return to top</a>
+        </div>
+        <div className="usa-footer__primary-section bg-primary-lighter" data-test="links">
+    <nav className="usa-footer__nav">
+    <ul className="grid-row grid-gap">
               {map(this.props.links, link => {
                 if (startsWith(link.url, 'http') || startsWith(link.url, 'mailto')) {
                   return (
-                    <li key={link.name}>
+                    <li key={link.name} className="mobile-lg:grid-col-4 desktop:grid-col-auto usa-footer__primary-content">
                       <a
-                        className="link"
+                        className="usa-footer__primary-link text-base-dark"
                         href={link.url}
                         target="_blank"
                         rel="noopener noreferrer"
@@ -27,44 +30,70 @@ export default class Footer extends PureComponent {
                   )
                 }
                 return (
-                  <li key={link.name}>
-                    <CustomLink to={link.url} className="link">
+                  <li key={link.name} className="mobile-lg:grid-col-4 desktop:grid-col-auto usa-footer__primary-content">
+                    <CustomLink to={link.url} className="usa-footer__primary-link text-base-dark">
                       <span>{link.name}</span>
                     </CustomLink>
                   </li>
                 )
               })}
             </ul>
-          </div>
-          <div className="footer-part logos" data-test="logos">
-            <ul>
+    </nav>
+  </div>
+
+
+          <div className="usa-footer__secondary-section bg-white" data-test="logos">
+          <div className="grid-container">
+      <div className="grid-row grid-gap">
+        <div className="usa-footer__logo grid-row mobile-lg:grid-col-6 mobile-lg:grid-gap-2">
+          <div className="mobile-lg:grid-col-auto">
+          <ul>
               {map(this.props.logos, logo => (
                 <li key={logo.name}>
-                  <a href={logo.url} target="_blank" rel="noopener noreferrer">
-                    <img src={logo.image} alt={logo.name} />
-                    <span>{logo.name}</span>
+                  <a href={logo.url} target="_blank" rel="noopener noreferrer" className="text-no-underline">
+                    <img src={logo.image} alt={logo.name} className="maxh-15"/>
+                    <div className="mobile-lg:grid-col-auto">
+                    <h3 className="usa-footer__logo-heading">{logo.name}</h3>
+                    </div>
                   </a>
                 </li>
               ))}
             </ul>
           </div>
-          <div className="footer-part socials" data-test="socials">
-            <ul>
+
+        </div>
+        <div className="usa-footer__contact-links mobile-lg:grid-col-6">
+            <ul className="usa-footer__social-links grid-row grid-gap-1" data-test="socials">
               {map(this.props.socials, social => (
-                <li key={social.name}>
+                <li key={social.name} className="grid-col-auto">
                   <a
                     href={social.url}
                     target="_blank"
                     rel="noopener noreferrer"
                     aria-label={social.name}
+                    className="font-body-2xl"
                   >
                     <div className={social.icon} />
                   </a>
                 </li>
               ))}
             </ul>
-          </div>
+          
+          
+          <address className="usa-footer__address">
+            <div className="usa-footer__contact-info grid-row grid-gap">
+              <div className="grid-col-auto">
+                <a href="mailto:info@agency.gov">code@gsa.gov</a>
+              </div>
+            </div>
+          </address>
         </div>
+      </div>
+    </div>
+
+          </div>
+          
+
       </footer>
     )
   }
