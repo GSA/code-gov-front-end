deb7c92dae4f3f194db619119b7a8e46dcae30b3
Sara Cope <sara.cope@gsa.gov>
2019-11-29 13:03:26 -0500

    Update home-about to use USWDS

--- src/components/home-about/home-about.component.js
+++ src/components/home-about/home-about.component.js
@@ -4,24 +4,24 @@ import CustomLink from 'components/custom-link'
 export default class HomeAbout extends React.Component {
   render() {
     return (
-      <section id="about" className="about tablet:grid-col-6">
-        <header>
-          <h2>About Code.gov</h2>
+      <section id="about" className="bg-primary-lighter padding-top-6 tablet:grid-col-6">
+        <header className="padding-x-4">
+          <h3 className="text-primary text-center">About Code.gov</h3>
           <p>{this.props.aboutus}</p>
         </header>
-        <ul className="about-actions">
+        <ul className="padding-x-4">
           <li>
-            <h3>Vision</h3>
-            <span className="about-icon">
-              <p>{this.props.vision}</p>
-              <img alt="" className="" src={`${PUBLIC_PATH}assets/img/icons/vision_icon_sm.png`} />
+            <h2 className="text-primary text-uppercase margin-left-8 margin-bottom-0">Vision</h2>
+            <span className="display-flex tablet:flex-row">
+              <p className="">{this.props.vision}</p>
+              <img alt="" className="order-first height-7 margin-top-2 margin-right-2" src={`${PUBLIC_PATH}assets/img/icons/vision_icon_sm.png`} />
             </span>
           </li>
           <li>
-            <h3>Mission</h3>
-            <span className="about-icon">
+            <h2 className="text-primary text-uppercase margin-left-8 margin-bottom-0">Mission</h2>
+            <span className="display-flex about-icon">
               <p>{this.props.mission}</p>
-              <img alt="" className="" src={`${PUBLIC_PATH}assets/img/icons/mission_icon_sm.png`} />
+              <img alt="" className="order-first height-7 margin-top-2 margin-right-2" src={`${PUBLIC_PATH}assets/img/icons/mission_icon_sm.png`} />
             </span>
           </li>
         </ul>
