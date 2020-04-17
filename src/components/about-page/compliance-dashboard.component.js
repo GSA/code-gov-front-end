9da009e3c49013fae6782a1a9dc399fcb8e0caa9
Brian Hurst <brian.hurst@gsa.gov>
2020-04-17 16:44:12 -0400

    Convert compliance dashboard web component to react component

--- src/components/about-page/compliance-dashboard.component.js
+++ src/components/about-page/compliance-dashboard.component.js
@@ -1,108 +1,76 @@
-import React, { Component, Fragment } from 'react'
-import { loadScript } from 'utils/other'
-import LazyHTML from 'components/lazy-html'
-import client from 'api-client'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-const onDashboard = [
-  'DHS',
-  'DOC',
-  'DOD',
-  'DOE',
-  'DOI',
-  'DOJ',
-  'DOL',
-  'DOS',
-  'DOT',
-  'ED',
-  'EPA',
-  'GSA',
-  'HHS',
-  'HUD',
-  'NASA',
-  'NRC',
-  'NSF',
-  'OPM',
-  'SBA',
-  'SSA',
-  'TREASURY',
-  'USAID',
-  'USDA',
-  'VA'
-]
-
-const dataurl = `${PUBLIC_PATH}src/components/about-page/html/`
-
-const configJSON = {
-  scores: {
-    compliant: [1, null],
-    partial: [0.25, 0.9999999],
-    noncompliant: [null, 0.244444444]
-  },
-  text: [
-    {
-      req: 'agencyWidePolicy',
-      variants: {
-        compliant: 'Agency policy is consistent with the Federal Source Code Policy.',
-        noncompliant:
-          'Agency policy has not been reviewed for consistency with the Federal Source Code Policy.',
-        partial:
-          'Agency policy is being updated for consistency with the Federal Source Code Policy.'
-      }
-    },
-    {
-      req: 'openSourceRequirement',
-      variants: {
-        compliant: 'Agency has open sourced greater than 20% of their custom developed code.',
-        noncompliant: 'Agency has open sourced less than 10% of their custom developed code.',
-        partial: 'Agency has open sourced greater than 10% of their custom developed code.'
-      }
-    },
-    {
-      req: 'inventoryRequirement',
-      variants: {
-        compliant: 'Agency has inventoried 100% of new custom code.',
-        noncompliant: 'Agency has inventoried less than 50% of new custom code.',
-        partial: 'Agency has inventoried more than 50% of new custom code.'
-      }
-    }
-  ]
+const displayStatus = {
+  compliant: 'Fully compliant',
+  noncompliant: 'Non-compliant',
+  partial: 'Partially compliant'
 }
 
-class ComplianceDashboard extends Component {
-  constructor(props) {
-    super(props)
-    this.loading = false
-    this.state = {}
-  }
+const getStatusAsText = (config, score) => {
+  const scores = config.scores
 
-  componentDidMount() {
-    if (!this.loading) {
-      const webcomponent = customElements.get('compliance-dashboard')
-      if (!webcomponent) {
-        loadScript(`${PUBLIC_PATH}webcomponents/compliance-dashboard.js`, true)
+  for (const status in scores) {
+    const [min, max] = scores[status]
+    if (typeof min === 'number' && typeof max === 'number') {
+      if (min <= score && score <= max) {
+        return status
       }
-      client.getCompliance().then(compliance => {
-        compliance = compliance.filter(agency => onDashboard.includes(agency.acronym))
-        compliance.forEach(agency => {
-          agency.img = `${PUBLIC_PATH}assets/img/logos/agencies/${agency.acronym}-50x50.png`
-        })
-        this.setState({ compliance })
-      })
+    } else if (typeof min === 'number' && typeof max !== 'number') {
+      if (min <= score) {
+        return status
+      }
+    } else if (typeof min !== 'number' && typeof max === 'number') {
+      if (score <= max) {
+        return status
+      }
     }
   }
-
-  render() {
-    return (
-      <Fragment>
-        <LazyHTML url={`${dataurl}compliance/agency-compliance.html`} />
-        <compliance-dashboard
-          id="compliance-dashboard"
-          config={JSON.stringify(configJSON)}
-          data={JSON.stringify(this.state.compliance)}
-        />
-      </Fragment>
-    )
-  }
+  return ''
 }
 
-export default ComplianceDashboard
+const getReqLine = (name, req, status, text) => (
+  <div key={`${name}-${req}`} className={`req ${status}`}>
+    {text}
+  </div>
+)
+
+const getCard = (config, entry) => {
+  const { img, name, requirements } = entry
+  const overallStatus = getStatusAsText(config, requirements.overall)
+
+  return (
+    <li className={`card ${overallStatus}`} key={`card-${name}`}>
+      <div className="dashboard-entity-icon">
+        <img src={img} alt={`${name} logo`} />
+      </div>
+      <div className="dashboard-entity-content">
+        <div className="dashboard-entity-heading">
+          <h3 className="h3">{name}</h3>
+          <h4 className={`h4 ${overallStatus}`}>{displayStatus[overallStatus]}</h4>
+          {config.text.map(textPart => {
+            const { req, variants } = textPart
+            const status = getStatusAsText(config, entry.requirements.sub[req])
+
+            return getReqLine(name, req, status, variants[status])
+          })}
+        </div>
+      </div>
+    </li>
+  )
+}
+
+const ComplianceDashboardComponent = props => (
+  <>
+    <div className="dashboard-container">
+      <ul className="dashboard-list">{props.data.map(entry => getCard(props.config, entry))}</ul>
+    </div>
+  </>
+)
+
+ComplianceDashboardComponent.propTypes = {
+  config: PropTypes.object.isRequired,
+  data: PropTypes.array.isRequired
+}
+
+export default ComplianceDashboardComponent
