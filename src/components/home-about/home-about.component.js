819c1a0b948c04a5110c48146ae6496863c3b603
Anna Zhang <annazhangg@yahoo.com>
2020-06-04 16:56:00 -0400

    update site banner and format news and about

--- src/components/home-about/home-about.component.js
+++ src/components/home-about/home-about.component.js
@@ -4,35 +4,37 @@ import CustomLink from 'components/custom-link'
 export default class HomeAbout extends React.Component {
   render() {
     return (
-      <section id="about" className="bg-primary-lighter tablet:grid-col-6 padding-right-2">
-        <header className="padding-x-2 padding-top-6">
-          <h2 className="font-heading-lg text-primary text-center">About Code.gov</h2>
-          <p>{this.props.aboutus}</p>
-        </header>
-        <ul className="padding-x-2">
-          <li>
-            <h3 className="text-primary text-uppercase margin-left-9 margin-bottom-0">Vision</h3>
-            <span className="display-flex tablet:flex-row margin-0">
-              <p className="">{this.props.vision}</p>
-              <img
-                alt=""
-                className="order-first height-7 margin-top-1 margin-right-2"
-                src={`${PUBLIC_PATH}assets/img/icons/vision_icon_sm.png`}
-              />
-            </span>
-          </li>
-          <li>
-            <h3 className="text-primary text-uppercase margin-left-9 margin-bottom-0">Mission</h3>
-            <span className="display-flex table:flex-row margin-0">
-              <p>{this.props.mission}</p>
-              <img
-                alt=""
-                className="order-first height-7 margin-top-1 margin-right-2"
-                src={`${PUBLIC_PATH}assets/img/icons/mission_icon_sm.png`}
-              />
-            </span>
-          </li>
-        </ul>
+      <section id="about" className="tablet:grid-col-6">
+        <div className="bg-primary-lighter height-full">
+          <header className="padding-x-4 padding-top-6">
+            <h2 className="font-heading-lg text-primary text-center">About Code.gov</h2>
+            <p>{this.props.aboutus}</p>
+          </header>
+          <ul className="padding-x-4">
+            <li className="padding-y-3">
+              <h3 className="text-primary text-uppercase margin-left-9 margin-bottom-0">Vision</h3>
+              <span className="display-flex tablet:flex-row margin-0">
+                <p className="">{this.props.vision}</p>
+                <img
+                  alt=""
+                  className="order-first height-7 margin-top-3 margin-right-2"
+                  src={`${PUBLIC_PATH}assets/img/icons/vision_icon_sm.png`}
+                />
+              </span>
+            </li>
+            <li>
+              <h3 className="text-primary text-uppercase margin-left-9 margin-bottom-0">Mission</h3>
+              <span className="display-flex table:flex-row margin-0">
+                <p>{this.props.mission}</p>
+                <img
+                  alt=""
+                  className="order-first height-7 margin-top-3 margin-right-2"
+                  src={`${PUBLIC_PATH}assets/img/icons/mission_icon_sm.png`}
+                />
+              </span>
+            </li>
+          </ul>
+        </div>
       </section>
     )
   }
--- src/components/home-news/home-news.component.js
+++ src/components/home-news/home-news.component.js
@@ -3,30 +3,32 @@ import React, { Component } from 'react'
 export default class HomeNews extends Component {
   render() {
     return (
-      <section className="bg-primary-lighter news tablet:grid-col-6 padding-left-2">
-        <header className="padding-x-4  padding-top-6">
-          <h2 className="font-heading-l text-primary text-center">Open Source News & Events</h2>
-        </header>
-        <ul className="padding-x-1 padding-bottom-2">
-          {this.props.newsItems &&
-            this.props.newsItems.map(news => (
-              <li className="bg-white grid-row margin-1 padding-2" key={news.title}>
-                <div className="flex-1">
-                  <h3 className="font-heading-sm text-bold">
-                    <a href={news.url} target="_blank" rel="noopener noreferrer">
-                      {news.title}
-                    </a>
-                  </h3>
-                  <p>{news.description}</p>
-                  <time>{news.date}</time>
-                </div>
-                <picture className="order-first padding-right-2">
-                  <source srcSet={news.image} media="min-width: 800px" />
-                  <img src={news.image} alt={news.alt} className="maxw-15 margin-right-1" />
-                </picture>
-              </li>
-            ))}
-        </ul>
+      <section className="tablet:grid-col-6">
+        <div className="bg-primary-lighter">
+          <header className="padding-x-4  padding-top-6">
+            <h2 className="font-heading-l text-primary text-center">Open Source News & Events</h2>
+          </header>
+          <ul className="padding-x-1 padding-bottom-2">
+            {this.props.newsItems &&
+              this.props.newsItems.map(news => (
+                <li className="bg-white grid-row margin-1 padding-2" key={news.title}>
+                  <div className="flex-1">
+                    <h3 className="font-heading-sm text-bold">
+                      <a href={news.url} target="_blank" rel="noopener noreferrer">
+                        {news.title}
+                      </a>
+                    </h3>
+                    <p>{news.description}</p>
+                    <time>{news.date}</time>
+                  </div>
+                  <picture className="order-first padding-right-2">
+                    <source srcSet={news.image} media="min-width: 800px" />
+                    <img src={news.image} alt={news.alt} className="maxw-15 margin-right-1" />
+                  </picture>
+                </li>
+              ))}
+          </ul>
+        </div>
       </section>
     )
   }
