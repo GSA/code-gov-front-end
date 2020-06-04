cb84df63097139ddbede4f3e549ed0c158a717d9
Anna Zhang <annazhangg@yahoo.com>
2020-06-04 16:56:00 -0400

    update site banner and format news and about

--- src/components/site-banner/site-banner.component.js
+++ src/components/site-banner/site-banner.component.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
 export default function SiteBanner({ backgroundImage, title }) {
-  return <simple-banner title={title} />
+  return (
+    <div className="bg-primary usa-section">
+      <div className="grid-container padding-top-7">
+        <div className="grid-row">
+          <div className="tablet:grid-col">
+            <h1 className="font-heading-3xl text-white text-center text-uppercase">{title}</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
