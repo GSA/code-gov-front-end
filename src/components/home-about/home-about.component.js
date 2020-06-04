028a89be1538b9c067eced02bf56a57c9befc943
Anna Zhang <annazhangg@yahoo.com>
2020-06-04 11:57:55 -0400

    work on theme fonts, format explore, about, and news on homepage

--- src/components/home-about/home-about.component.js
+++ src/components/home-about/home-about.component.js
@@ -4,24 +4,32 @@ import CustomLink from 'components/custom-link'
 export default class HomeAbout extends React.Component {
   render() {
     return (
-      <section id="about" className="bg-primary-lighter tablet:grid-col-6">
-        <header className="padding-x-4 padding-top-6">
-          <h2 className="font-heading-l text-primary text-center">About Code.gov</h2>
+      <section id="about" className="bg-primary-lighter tablet:grid-col-6 padding-right-2">
+        <header className="padding-x-2 padding-top-6">
+          <h2 className="font-heading-lg text-primary text-center">About Code.gov</h2>
           <p>{this.props.aboutus}</p>
         </header>
-        <ul className="padding-x-4">
+        <ul className="padding-x-2">
           <li>
-            <h3 className="text-primary text-uppercase margin-left-8 margin-bottom-0">Vision</h3>
-            <span className="display-flex tablet:flex-row">
+            <h3 className="text-primary text-uppercase margin-left-9 margin-bottom-0">Vision</h3>
+            <span className="display-flex tablet:flex-row margin-0">
               <p className="">{this.props.vision}</p>
-              <img alt="" className="order-first height-7 margin-top-2 margin-right-2" src={`${PUBLIC_PATH}assets/img/icons/vision_icon_sm.png`} />
+              <img
+                alt=""
+                className="order-first height-7 margin-top-1 margin-right-2"
+                src={`${PUBLIC_PATH}assets/img/icons/vision_icon_sm.png`}
+              />
             </span>
           </li>
           <li>
-            <h3 className="text-primary text-uppercase margin-left-8 margin-bottom-0">Mission</h3>
-            <span className="display-flex about-icon">
+            <h3 className="text-primary text-uppercase margin-left-9 margin-bottom-0">Mission</h3>
+            <span className="display-flex table:flex-row margin-0">
               <p>{this.props.mission}</p>
-              <img alt="" className="order-first height-7 margin-top-2 margin-right-2" src={`${PUBLIC_PATH}assets/img/icons/mission_icon_sm.png`} />
+              <img
+                alt=""
+                className="order-first height-7 margin-top-1 margin-right-2"
+                src={`${PUBLIC_PATH}assets/img/icons/mission_icon_sm.png`}
+              />
             </span>
           </li>
         </ul>
--- src/components/home-explore/home-explore.component.js
+++ src/components/home-explore/home-explore.component.js
@@ -4,28 +4,33 @@ import CustomLink from 'components/custom-link'
 export default class HomeExplore extends React.Component {
   render() {
     return (
-      <section id="explore" className="explore usa-section">
-        <div className="explore-actions grid-row text-center tablet:border-bottom-2px tablet:border-top-2px padding-top-205 padding-bottom-205">
-          {this.props.exploreItems &&
-            this.props.exploreItems.map(item => (
-              <div className="explore-action grid-row flex-column flex-justify  padding-right-2 padding-left-2 tablet:grid-col-4 tablet:border-right-2px" key={item.title}>
-                <div>
-                  <h2>{item.title}</h2>
-                  <p dangerouslySetInnerHTML={{ __html: item.description }} />
+      <div className="grid-row">
+        <section id="explore" className="explore usa-section">
+          <div className="explore-actions grid-row text-center tablet:border-bottom-2px tablet:border-top-2px padding-top-205 padding-bottom-205">
+            {this.props.exploreItems &&
+              this.props.exploreItems.map(item => (
+                <div
+                  className="explore-action grid-row flex-column flex-justify  padding-right-2 padding-left-2 tablet:grid-col-4 tablet:border-right-2px"
+                  key={item.title}
+                >
+                  <div>
+                    <h2 className="text-primary">{item.title}</h2>
+                    <p dangerouslySetInnerHTML={{ __html: item.description }} />
+                  </div>
+                  <div className="buttons">
+                    {item.links.map(link => (
+                      <span key={link.name}>
+                        <a href={link.url}>
+                          <button className="alt">{link.name}</button>
+                        </a>
+                      </span>
+                    ))}
+                  </div>
                 </div>
-                <div className="buttons">
-                  {item.links.map(link => (
-                    <span key={link.name}>
-                      <a href={link.url}>
-                        <button className="alt">{link.name}</button>
-                      </a>
-                    </span>
-                  ))}
-                </div>
-              </div>
-            ))}
-        </div>
-      </section>
+              ))}
+          </div>
+        </section>
+      </div>
     )
   }
 }
