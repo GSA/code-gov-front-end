c2093158393e4cd4808534f7e19e7a5e34cdee20
Sean Stares <sean.starne@gsa.gov>
2020-06-10 09:50:17 -0500

    Added force to prevent race condition on menu load. @todo expanded is not getting passed to PrimaryDropdown or SecondaryDropdown when mobile menu is expanded

--- cypress/integration/about-page.spec.js
+++ cypress/integration/about-page.spec.js
@@ -1,17 +1,18 @@
 describe('about page tests', () => {
   beforeEach(() => {
     cy.visit('/')
-      .get('li a[role="menuitem"]')
+  })
+  it('selects the about menu item', () => {
+    cy.get('li a[role="menuitem"]')
       .contains(/about/i)
       .click()
   })
   it('visits the about/overview page', () => {
     cy.get('ul[role="menu"] > li > a')
       .contains(/overview/i)
-      .click()
+      .click({ force: true })
       .get('#aboutthesourcecodepolicy')
       .contains(/source code/i)
       .should('exist')
   })
-
 })
