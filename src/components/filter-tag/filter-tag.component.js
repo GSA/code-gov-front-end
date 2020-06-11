cb03c23b261895ea6ebf62722400794ecc31cd45
Anna Zhang <annazhangg@yahoo.com>
2020-06-11 11:53:13 -0400

    align filter boxes with cards for browse projects and open tasks, get rid of current filter-tag styling

--- src/components/filter-tag/filter-tag.component.js
+++ src/components/filter-tag/filter-tag.component.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 export default function FilterTag({ category, onClick, title, value }) {
-  console.log("starting to render FilterTag with", [category, title, value])
+  console.log('starting to render FilterTag with', [category, title, value])
   return (
-    <div className="filter-tag" onClick={() => onClick(category, value)}>
+    <div
+      className="filter-tag margin-top-3 margin-right-2"
+      onClick={() => onClick(category, value)}
+    >
       <div className="filter-tag-title">{title}</div>
     </div>
   )
