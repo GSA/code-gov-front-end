b026746fa24e39ea4ef7038950074ff5a142a15c
Anna Zhang <annazhangg@yahoo.com>
2020-06-11 16:19:16 -0400

    fix header tags, add color, format news

--- src/components/home-news/home-news.component.js
+++ src/components/home-news/home-news.component.js
@@ -6,7 +6,9 @@ export default class HomeNews extends Component {
       <section className="tablet:grid-col-6">
         <div className="bg-primary-lighter">
           <header className="padding-x-4  padding-top-6">
-            <h2 className="font-heading-l text-primary text-center">Open Source News & Events</h2>
+            <h2 className="font-heading-l text-primary-darker text-center">
+              Open Source News & Events
+            </h2>
           </header>
           <ul className="padding-x-1 padding-bottom-2">
             {this.props.newsItems &&
@@ -18,8 +20,8 @@ export default class HomeNews extends Component {
                         {news.title}
                       </a>
                     </h3>
-                    <p>{news.description}</p>
-                    <time>{news.date}</time>
+                    <p className="margin-bottom-0 font-body-2xs margin-top-105">{news.description}</p>
+                    <time className="font-body-3xs">{news.date}</time>
                   </div>
                   <picture className="order-first padding-right-2">
                     <source srcSet={news.image} media="min-width: 800px" />
