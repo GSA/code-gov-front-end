880e4b47c5b055a5998d97cf3e2ae78afaa74203
Anna Zhang <annazhangg@yahoo.com>
2020-06-29 16:28:08 -0400

    get rid of _breadcrumbs.scss and reformat breadcrumbs

--- src/components/breadcrumbs/breadcrumbs.component.js
+++ src/components/breadcrumbs/breadcrumbs.component.js
@@ -4,17 +4,23 @@ import CustomLink from 'components/custom-link'
 export default function Breadcrumbs({ crumbs }) {
   return (
     <div className="grid-container">
-      <ul className="breadcrumbs grid-row margin-top-4">
-        {crumbs.map(({ text, to }) => {
-          if (to) {
+      <ul className="grid-row margin-top-4">
+        {crumbs
+          .map(({ text, to }) => {
+            if (to) {
+              return (
+                <li key={text} className="padding-right-1">
+                  <CustomLink to={to}>{text}</CustomLink>
+                </li>
+              )
+            }
             return (
-              <li key={text}>
-                <CustomLink to={to}>{text}</CustomLink>
+              <li key={text} className="padding-left-1 text-bold">
+                {text}
               </li>
             )
-          }
-          return <li key={text}>{text}</li>
-        })}
+          })
+          .reduce((prev, curr) => [prev, '> ', curr])}
       </ul>
     </div>
   )
