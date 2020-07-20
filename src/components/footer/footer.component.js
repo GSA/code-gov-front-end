6b4824b8905468443e4d6a5d68ec512f41a18a0e
Anna Zhang <annazhangg@yahoo.com>
2020-07-20 17:09:44 -0400

    use uswds to create dynamic footer switch, update footer snapshot

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -13,17 +13,17 @@ export default class Footer extends PureComponent {
           </a>
         </div>
         <div className="usa-footer__primary-section bg-primary-lighter" data-test="links">
-          <nav className="usa-footer__nav padding-top-4 tablet:padding-top-0 border-bottom-0">
+          <nav className="usa-footer__nav padding-top-4 tablet-lg:padding-top-0 border-bottom-0">
             <ul className="grid-row grid-gap">
               {map(this.props.links, link => {
                 if (startsWith(link.url, 'http') || startsWith(link.url, 'mailto')) {
                   return (
                     <li
                       key={link.name}
-                      className="grid-col-12 tablet:grid-col-auto usa-footer__primary-content font-body-3xs margin-bottom-0 border-top-0"
+                      className="grid-col-12 tablet-lg:grid-col-auto usa-footer__primary-content font-body-3xs margin-bottom-0 border-top-0"
                     >
                       <a
-                        className="usa-footer__primary-link padding-bottom-1 padding-top-0 tablet:padding-y-2 text-base-dark"
+                        className="usa-footer__primary-link padding-bottom-1 padding-top-0 tablet-lg:padding-y-2 text-base-dark"
                         href={link.url}
                         target="_blank"
                         rel="noopener noreferrer"
@@ -37,11 +37,11 @@ export default class Footer extends PureComponent {
                 return (
                   <li
                     key={link.name}
-                    className="grid-col-12 tablet:grid-col-auto usa-footer__primary-content font-body-3xs margin-bottom-0 border-top-0"
+                    className="grid-col-12 tablet-lg:grid-col-auto usa-footer__primary-content font-body-3xs margin-bottom-0 border-top-0"
                   >
                     <CustomLink
                       to={link.url}
-                      className="usa-footer__primary-link padding-bottom-1 padding-top-0 tablet:padding-y-2 text-base-dark"
+                      className="usa-footer__primary-link padding-bottom-1 padding-top-0 tablet-lg:padding-y-2 text-base-dark"
                     >
                       <span className="text-base-dark">{link.name}</span>
                     </CustomLink>
@@ -58,32 +58,30 @@ export default class Footer extends PureComponent {
         >
           <div className="grid-container">
             <div className="grid-row grid-gap">
-              <div className="usa-footer__logo grid-row grid-col-12 tablet-lg:grid-col-8">
-                <div className="mobile-lg:grid-col-auto">
-                  {map(this.props.logos, logo => (
-                    <div key={logo.name}>
-                      <a
-                        href={logo.url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-no-underline"
-                      >
-                        <img
-                          src={logo.image}
-                          alt={`${logo.name} logo`}
-                          className="maxw-15 padding-right-2 show-w-gt-tablet-lg"
-                        />
-                        <div className="mobile-lg:grid-col-auto desktop:display-inline">
-                          <h3 className="usa-footer__logo-heading usa-footer__primary-link font-body-3xs text-primary text-bold desktop:display-inline padding-left-0">
-                            {logo.name}
-                          </h3>
-                        </div>
-                      </a>
-                    </div>
-                  ))}
-                </div>
+              <div className="usa-footer__logo grid-col-12 tablet-lg:grid-col-8">
+                {map(this.props.logos, logo => (
+                  <div key={logo.name}>
+                    <a
+                      href={logo.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-no-underline"
+                    >
+                      <img
+                        src={logo.image}
+                        alt={`${logo.name} logo`}
+                        className="maxw-15 padding-right-2 display-none tablet-lg:display-inline"
+                      />
+                      <div className="mobile-lg:grid-col-auto tablet-lg:display-inline">
+                        <h3 className="usa-footer__logo-heading usa-footer__primary-link font-body-3xs text-primary text-bold tablet-lg:display-inline padding-left-0">
+                          {logo.name}
+                        </h3>
+                      </div>
+                    </a>
+                  </div>
+                ))}
               </div>
-              <div className="usa-footer__contact-links tablet-lg:grid-offset-1 tablet-lg:padding-top-7 text-left tablet-lg:grid-col-3">
+              <div className="usa-footer__contact-links tablet-lg:grid-offset-1 tablet-lg:padding-top-5 text-left tablet-lg:grid-col-3">
                 <p className="padding-right-1 margin-top-0 font-body-3xs tablet-lg:grid-col-6 tablet-lg:grid-offset-6 grid-col-12">
                   <a href="mailto:code@gsa.gov">code@gsa.gov</a>
                 </p>
@@ -94,7 +92,7 @@ export default class Footer extends PureComponent {
                   {map(this.props.socials, social => (
                     <li
                       key={social.name}
-                      className="grid-col-12 tablet-lg:grid-col-3 desktop:margin-top-neg-1 margin-top-1"
+                      className="grid-col-12 tablet-lg:grid-col-3 tablet-lg:margin-top-neg-1 margin-top-1"
                     >
                       <a
                         href={social.url}
