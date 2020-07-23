c21032e77903151acb810f58c280b38d4049a327
Anna Zhang <annazhangg@yahoo.com>
2020-07-23 18:32:37 -0400

    fix keypress/click triggers for checkbox and show more/show less for filter-box

--- src/components/filter-box-web/filter-box.component.js
+++ src/components/filter-box-web/filter-box.component.js
@@ -4,17 +4,20 @@ export default class FilterBoxWeb extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      showAll: false
+      showAll: false,
+      accordionExpand: true
     }
     this.handleChange = this.handleChange.bind(this)
-    this.handleClick = this.handleClick.bind(this)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+    this.handleKeyUp = this.handleKeyUp.bind(this)
+    this.handleClickToggle = this.handleClickToggle.bind(this)
+    this.handleExpand = this.handleExpand.bind(this)
   }
 
   get filterOptions() {
     return (
       <>
         {JSON.parse(this.props.options).map((option, index) => {
-          let className = ''
           const optionName = option.name
           const optionValue = option.value
           const optionChecked = option.checked
@@ -24,12 +27,9 @@ export default class FilterBoxWeb extends Component {
           } else {
             hidden.hidden = true
           }
-          // let hidden = ''
-          if (option.checked) className += ' checked'
-          if (index >= 4) className += 'hideOnCollapsed'
           if (index >= 4) {
             return (
-              <div className={`usa-checkbox ${className}`} {...hidden}>
+              <div className="usa-checkbox" {...hidden} key={optionName}>
                 <input
                   type="checkbox"
                   className="usa-checkbox__input"
@@ -46,74 +46,31 @@ export default class FilterBoxWeb extends Component {
                 </label>
               </div>
             )
-          } 
-            return (
-              <div className={`usa-checkbox ${className}`}>
-                <input
-                  type="checkbox"
-                  className="usa-checkbox__input"
-                  id={optionValue}
-                  value={optionValue}
-                  onChange={this.handleChange}
-                  checked={optionChecked}
-                />
-                <label
-                  className="usa-checkbox__label font-body-2xs text-base-dark"
-                  htmlFor={optionValue}
-                >
-                  <span>{optionName}</span>
-                </label>
-              </div>
-            )
-          
+          }
+          return (
+            <div className="usa-checkbox" key={optionName}>
+              <input
+                type="checkbox"
+                className="usa-checkbox__input"
+                id={optionValue}
+                value={optionValue}
+                onChange={this.handleChange}
+                checked={optionChecked}
+              />
+              <label
+                className="usa-checkbox__label font-body-2xs text-base-dark"
+                htmlFor={optionValue}
+              >
+                <span>{optionName}</span>
+              </label>
+            </div>
+          )
         })}
       </>
     )
   }
 
-  // componentDidMount() {
-  //   let showFields = true
-  //   if (document.querySelector('.moreLessToggle')) {
-  //     document.querySelectorAll('.moreLessToggle').forEach(button => button.addEventListener(
-  //       'click',
-  //       function(_) {
-  //         console.log(showFields)
-  //         // _this3.toggleState()
-  //         if (showFields) {
-  //           console.log(`coming into true: ${showFields}`)
-  //           showFields = !showFields
-  //           document.querySelectorAll('.hideOnCollapsed').forEach(field => {
-  //             field.setAttribute('hidden', 'true')
-  //           })
-  //           document.querySelector('.moreLessToggle').innerHTML = 'Show more'
-  //           console.log(`exiting from true as :${showFields}`)
-  //         } else if (showFields === undefined) {
-  //           console.log(`coming into undefined: ${showFields}`)
-  //           showFields = true
-  //           document.querySelectorAll('.hideOnCollapsed').forEach(field => {
-  //             field.removeAttribute('hidden')
-  //           })
-  //           document.querySelector('.moreLessToggle').innerHTML = 'Show less'
-  //           console.log(`exiting from undefined as :${showFields}`)
-  //         } else {
-  //           console.log(`coming into false: ${showFields}`)
-  //           showFields = !showFields
-  //           document.querySelectorAll('.hideOnCollapsed').forEach(field => {
-  //             field.removeAttribute('hidden')
-  //           })
-  //           document.querySelector('.moreLessToggle').innerHTML = 'Show less'
-  //           console.log(`exiting from false as :${showFields}`)
-  //         }
-  //       },
-  //       false
-  //     ))
-  //   }
-  // }
-
   handleChange(event) {
-    console.log(`handling change:${event.target.tagName}`)
-    console.log(`type:${event.target.checked}`)
-    console.log(`value:${event.target.value}`)
     if (event.target.tagName.toLowerCase() === 'input') {
       const type = event.target.checked ? 'checked' : 'unchecked'
       const value = event.target.value
@@ -121,54 +78,83 @@ export default class FilterBoxWeb extends Component {
     }
   }
 
-  handleClick(event) {
-    console.log(`handleClick:${  event.target}`)
-    console.log(`this is:${  this}`)
+  handleKeyDown(event) {
+    if (event.keyCode === 32) {
+      event.preventDefault()
+    } else if (event.keyCode === 13) {
+      const showAll = !this.state.showAll
+      this.setState({ showAll })
+    }
+  }
+
+  handleKeyUp(event) {
+    if (event.keyCode === 32) {
+      const showAll = !this.state.showAll
+      this.setState({ showAll })
+    }
+  }
+
+  handleClickToggle(event) {
     event.preventDefault()
     const showAll = !this.state.showAll
     this.setState({ showAll })
-    // if (event.target.tagName.toLowerCase() === 'input') {
-    //   const type = event.target.checked ? 'checked' : 'unchecked'
-    //   const value = event.target.value
-    //   this.props.eventChange({ type, value })
-    // }
+  }
+
+  handleExpand(event) {
+    const accordionExpand = !this.state.accordionExpand
+    this.setState({ accordionExpand })
   }
 
   render() {
     const title = this.props.title
+    const accordionContent = {}
+    if (this.state.accordionExpand) {
+      accordionContent.hidden = false
+    } else {
+      accordionContent.hidden = true
+    }
     return (
-      <>
+      <div className="usa-accordion border-base-light border-1px tablet-lg:margin-bottom-2 margin-bottom-0">
         <div className="usa-accordion__heading">
           <button
             className="usa-accordion__button font-heading-md text-base-darker"
-            aria-expanded="true"
+            aria-expanded={this.state.accordionExpand}
             aria-controls={title}
             id={`button-${title}`}
+            onClick={this.handleExpand}
           >
             {title}
           </button>
         </div>
-        <div id={title} className="usa-accordion__content usa-prose">
+        <div
+          id={title}
+          className="usa-accordion__content usa-prose border-top-1px border-base-light"
+          {...accordionContent}
+        >
           <form className="usa-form maxw-none">
             <fieldset className="usa-fieldset" aria-labelledby={`button-${title}`}>
               <legend className="usa-sr-only" aria-labelledby={`button-${title}`} />
               {this.filterOptions}
               {JSON.parse(this.props.options).length > 4 ? (
-                <button
-                  className="moreLessToggle text-underline text-primary font-body-2xs bg-white border-0 padding-0"
-                  // role="button"
-                  // tabIndex="0"
-                  onClick={this.handleClick}
-                >
-                  {this.state.showAll ? 'Show less' : 'Show more'}
-                </button>
+                <div>
+                  <span
+                    className="moreLessToggle text-underline text-primary font-body-2xs"
+                    onKeyDown={this.handleKeyDown}
+                    onKeyUp={this.handleKeyUp}
+                    onClick={this.handleClickToggle}
+                    role="button"
+                    tabIndex="0"
+                  >
+                    {this.state.showAll ? 'Show less' : 'Show more'}
+                  </span>
+                </div>
               ) : (
                 ''
               )}
             </fieldset>
           </form>
         </div>
-      </>
+      </div>
     )
   }
 }
--- src/components/filter-box/filter-box.component.js
+++ src/components/filter-box/filter-box.component.js
@@ -2,25 +2,6 @@ import React from 'react'
 import FilterBoxWeb from '../filter-box-web/filter-box.component'
 
 export default class FilterBox extends React.Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.FilterBoxWeb = React.createRef()
-  // }
-  // componentDidMount() {
-  //   // react doesn't bubble up custom events, so we have to do this
-  //   this.FilterBoxWeb.current.addEventListener('change', event => {
-  //     if (event.target.tagName.toLowerCase() === 'input') {
-  //       const type = event.target.checked ? 'checked' : 'unchecked'
-  //       const value = event.target.value
-  //       this.props.onChange({ type, value })
-  //     }
-  //   })
-  // }
-
-  // shouldComponentUpdate(nextProps) {
-  //   return JSON.stringify(nextProps) !== JSON.stringify(this.props)
-  // }
-
   render() {
     return (
       <FilterBoxWeb
@@ -28,6 +9,7 @@ export default class FilterBox extends React.Component {
         options={JSON.stringify(this.props.options)}
         ref="filterBox"
         eventChange={this.props.onChange}
+        key={`${this.props.title  } Filter`}
       />
     )
   }
