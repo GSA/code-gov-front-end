a06c8c9514a37ea64e31beef446c119e1c4b1b66
Anna Zhang <annazhangg@yahoo.com>
2020-07-24 14:11:32 -0400

    update filter-box and filter-boxes snapshots, update filter-box jest tests

--- src/components/filter-box/filter-box.component.test.js
+++ src/components/filter-box/filter-box.component.test.js
@@ -5,57 +5,106 @@ import FilterBox from 'components/filter-box/filter-box.component'
 
 const props = {
   title: 'test-title',
-  options: [
+  options: JSON.stringify([
     { id: 1, value: 'opt-1' },
-    { id:2, value: 'opt-2' },
-  ],
-  onChange: jest.fn(),
+    { id: 2, value: 'opt-2' },
+    { id: 3, value: 'opt-3' },
+    { id: 4, value: 'opt-4' },
+    { id: 5, value: 'opt-5' }
+  ]),
+  onChange: jest.fn()
 }
 
 let wrapper
 let instance
 describe('components - FilterBox', () => {
   beforeEach(() => {
-    // uses 'refs' must mount instead of shallow
     wrapper = mount(<FilterBox {...props} />)
     instance = wrapper.instance()
   })
 
-  describe('componentDidMount', () => {
-    it('should attach a change handler for the fitler box', () => {
-      // override read only tagName value...
-      Object.defineProperty(wrapper.ref('filterBox'), 'tagName', { value: 'input' })
-      wrapper.ref('filterBox').value = 'test-value'
-      wrapper.ref('filterBox').dispatchEvent(new Event('change'))
+  describe('onChange', () => {
+    it('should be called for checkbox input onChange', () => {
+      const event = {
+        target: { checked: false, value: 'test-value', tagName: 'INPUT' }
+      }
+      const input = wrapper.find('input[value="opt-1"]')
+      input.simulate('change', event)
       expect(props.onChange).toBeCalledWith({ type: 'unchecked', value: 'test-value' })
     })
+  })
 
-    it('should attach determine the `type` to change by the `checked` attribute', () => {
-      Object.defineProperty(wrapper.ref('filterBox'), 'tagName', { value: 'input' })
-      wrapper.ref('filterBox').value = 'test-value'
-      wrapper.ref('filterBox').checked = true
-      wrapper.ref('filterBox').dispatchEvent(new Event('change'))
-      expect(props.onChange).toBeCalledWith({ type: 'checked', value: 'test-value' })
+  describe('handleKeyDown', () => {
+    it('should inverse the value of showAll state if enter key is pressed', () => {
+      const event = {
+        keyCode: 13
+      }
+      wrapper.setState({ showAll: false })
+
+      // false => true
+      instance.handleKeyDown(event)
+      expect(wrapper.state('showAll')).toBeTruthy()
+
+      // true => false
+      instance.handleKeyDown(event)
+      expect(wrapper.state('showAll')).toBeFalsy()
     })
-
-    it('should only call `onChange` if the tag is an `input`', () => {
-      Object.defineProperty(wrapper.ref('filterBox'), 'tagName', { value: 'p' })
-      wrapper.ref('filterBox').dispatchEvent(new Event('change'))
-      expect(props.onChange).not.toBeCalled()
+    it('should call preventDefault if space bar is pressed', () => {
+      const event = {
+        keyCode: 32,
+        preventDefault: jest.fn()
+      }
+      instance.handleKeyDown(event)
+      expect(event.preventDefault).toBeCalled()
     })
   })
 
-  describe('shouldComponentUpdate', () => {
-    it('should update if next props are not current props', () => {
-      const nextProps = { ...props, options: [{ id: 99, value: 'opt-99' }] }
-      expect(instance.shouldComponentUpdate(nextProps)).toBeTruthy()
-    })
+  describe('handleKeyUp', () => {
+    it('should inverse the value of showAll state if space bar is pressed', () => {
+      const event = {
+        keyCode: 32
+      }
+      wrapper.setState({ showAll: false })
 
-    it('should not update if next props are the same as current props', () => {
-      expect(instance.shouldComponentUpdate(props)).toBeFalsy()
+      // false => true
+      instance.handleKeyUp(event)
+      expect(wrapper.state('showAll')).toBeTruthy()
+
+      // true => false
+      instance.handleKeyUp(event)
+      expect(wrapper.state('showAll')).toBeFalsy()
     })
   })
 
+  describe('handleClickToggle', () => {
+    it('should inverse the value of showAll state if moreLessToggle is clicked', () => {
+      const event = {
+        preventDefault: jest.fn()
+      }
+      wrapper.setState({ showAll: false })
+
+      // false => true
+      instance.handleClickToggle(event)
+      expect(wrapper.state('showAll')).toBeTruthy()
+
+      // true => false
+      instance.handleClickToggle(event)
+      expect(wrapper.state('showAll')).toBeFalsy()
+    })
+  })
+
+  describe('handleExpand', () => {
+    it('should inverse the value of accordionExpand state if the accordion title button is clicked', () => {
+      wrapper.setState({ accordionExpand: true })
+      // true => false
+      instance.handleExpand()
+      expect(wrapper.state('accordionExpand')).toBeFalsy()
+
+      // false => true
+      instance.handleExpand()
+      expect(wrapper.state('accordionExpand')).toBeTruthy()
+    })
+  })
   describe('render', () => {
     it('should render correctly', () => {
       expect(wrapper).toMatchSnapshot()
