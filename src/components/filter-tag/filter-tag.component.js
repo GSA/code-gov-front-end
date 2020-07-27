ffda164125c5d32491808f8e1a0cba38f2aa3be6
Anna Zhang <annazhangg@yahoo.com>
2020-07-27 10:29:21 -0400

    fix padding for icon on filter tag, update snapshot

--- src/components/filter-tag/filter-tag.component.js
+++ src/components/filter-tag/filter-tag.component.js
@@ -7,7 +7,7 @@ export default function FilterTag({ category, onClick, title, value }) {
       className="usa-button width-auto margin-top-3 margin-right-2 padding-1 bg-primary display-inline-block radius-md font-body-3xs padding-105 text-no-uppercase"
       onClick={() => onClick(category, value)}
     >
-      <i className="icon icon-close padding-right-2px" />
+      <i className="icon icon-close padding-right-05" />
       {title}
     </button>
   )
