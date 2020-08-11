6b905f870f4a3a9e1d542e1dfe00abcb4c4ebfd8
Joseph Castle <joseph.castle@gsa.gov>
2020-08-11 13:10:57 -0400

    Component updates

--- src/components/home-metrics/home-metrics.component.js
+++ src/components/home-metrics/home-metrics.component.js
@@ -3,29 +3,30 @@ import React, { Component } from 'react'
 export default class HomeMetrics extends Component {
   render() {
     return (
-      <div className="grid-row">
         <section id="metrics" className="metrics usa-section">
-          {this.props.metrics && this.props.metrics.map(item => ( 
-            <h2 className="text-primary">{item.title}</h2>
-            
-
-            <div className="metrics-actions grid-row 
-            tablet:border-bottom-2px tablet:border-top-2px tablet:padding-top-205 padding-top-3 tablet:padding-bottom-205 padding-bottom-0"
-            >  
-
-            </div> 
-            
-            
-            <div>
-              <h2 className="text-primary">{item.title}</h2>
-              <p dangerouslySetInnerHTML={{ __html: item.description }} />
-            </div>
-          ))}
-      
-      
         
+        <h2 className="text-primary">Federal Source Code Metrics</h2>
+        <h3>Last updated: 6/1/2020</h3>
+        
+        <div className="metrics-actions tablet:border-bottom-2px tablet:border-top-2px tablet:padding-top-205 tablet:padding-bottom-205 padding-bottom-0">
+        
+        <ul>
+          <li>Agencies: 26</li>
+          <li>Repositories: 7,911</li>
+          <li>Forks: 51,612</li>
+          <li>Stars & watchers: 113,741</li>
+          <li>Open issues: 38,566</li>
+          <li>Open source software: 0%</li>
+          <li>Government-wide reuse: 0%</li>
+        </ul>
+        
+        </div>
+
+        <div class="text-base-darker display-inline-block icon icon-office-building"></div>
+
+        <h3>*Data collected through GitHub API.</h3>
+          
       </section>
-    </div>
     )
   }
 }
