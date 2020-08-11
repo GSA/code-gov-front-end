aeaafedf2124e513d8aced11a46d09e9ea7b1a53
Joseph Castle <joseph.castle@gsa.gov>
2020-08-11 11:36:58 -0400

    Updates to metrics

--- src/components/home-metrics/home-metrics.component.js
+++ src/components/home-metrics/home-metrics.component.js
@@ -3,15 +3,29 @@ import React, { Component } from 'react'
 export default class HomeMetrics extends Component {
   render() {
     return (
-      <section className="tablet:grid-col-12">
-        <div className="bg-primary-lighter">
-          <header className="padding-x-4  padding-top-6">
-            <h2 className="font-heading-l text-primary-darker text-center">
-              Federal Source Code Metrics
-            </h2>
-          </header>
-        </div>
+      <div className="grid-row">
+        <section id="metrics" className="metrics usa-section">
+          {this.props.metrics && this.props.metrics.map(item => ( 
+            <h2 className="text-primary">{item.title}</h2>
+            
+
+            <div className="metrics-actions grid-row 
+            tablet:border-bottom-2px tablet:border-top-2px tablet:padding-top-205 padding-top-3 tablet:padding-bottom-205 padding-bottom-0"
+            >  
+
+            </div> 
+            
+            
+            <div>
+              <h2 className="text-primary">{item.title}</h2>
+              <p dangerouslySetInnerHTML={{ __html: item.description }} />
+            </div>
+          ))}
+      
+      
+        
       </section>
+    </div>
     )
   }
 }
