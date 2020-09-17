25e43fb4f36b4dd6520017cc67c4443ed1967d42
Sean Stares <sean.starne@gsa.gov>
2020-09-17 10:23:25 -0500

    added cssnano

--- config/webpack/webpack.shared.js
+++ config/webpack/webpack.shared.js
@@ -1,33 +1,29 @@
+const path = require('path')
 const { dirname, join, resolve } = require('path')
 const AppManifestWebpackPlugin = require('app-manifest-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ImageminPlugin = require('imagemin-webpack-plugin').default
 const DefinePlugin = require('webpack/lib/DefinePlugin')
 const EnvironmentPlugin = require('webpack/lib/EnvironmentPlugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const sass = require('sass')
-const ImageminPlugin = require('imagemin-webpack-plugin').default
 
 const rootDir = dirname(dirname(__dirname))
+const sass = require('sass')
+
 const nodeModulesDir = join(rootDir, 'node_modules')
-const uswdsContext = resolve(rootDir, 'node_modules', 'uswds', 'src', 'fonts')
+const siteConfigPath = process.env.CODE_GOV_CONFIG_JSON || join(rootDir, '/config/site/site.json')
+const SITE_CONFIG = require(siteConfigPath)
 
 require('dotenv-flow').config()
 
-// https://webpack.js.org/guides/public-path/
 let PUBLIC_PATH = process.env.BASEURL || process.env.PUBLIC_PATH || '/'
 let OUTPUT_PATH
+const BRANCH = process.env.BRANCH || 'development'
 
 // add slash to end of path for federalist branch builds
 if (PUBLIC_PATH.slice(-1) !== '/') PUBLIC_PATH = `${PUBLIC_PATH}/`
-
-console.log('process.env.CODE_GOV_API_BASE:', process.env.CODE_GOV_API_BASE)
-console.log('process.env.CODE_GOV_API_KEY:', process.env.CODE_GOV_API_KEY)
-console.log('process.env.CODE_GOV_BRANCH:', process.env.CODE_GOV_BRANCH)
-console.log('process.env.CODE_GOV_RELATIVE_DIR:', process.env.CODE_GOV_RELATIVE_DIR)
-console.log('process.env.PUBLIC_PATH', process.env.PUBLIC_PATH)
-console.log('PUBLIC_PATH', PUBLIC_PATH)
-console.log('process.env.BASEURL:', process.env.BASEURL)
 
 if (process.env.OUTPUT_PATH) {
   OUTPUT_PATH = process.env.OUTPUT_PATH
@@ -37,92 +33,7 @@ if (process.env.OUTPUT_PATH) {
   OUTPUT_PATH = join(rootDir, '/dist')
 }
 
-console.log('OUTPUT_PATH:', OUTPUT_PATH)
-
-if (!OUTPUT_PATH) {
-  throw new Error('Please define an output path')
-}
-
-const entry = {
-  index: ['@babel/polyfill', './src/index.js']
-}
-
-const siteConfigPath = process.env.CODE_GOV_CONFIG_JSON || join(rootDir, '/config/site/site.json')
-
-/* eslint-disable import/no-dynamic-require */
-const SITE_CONFIG = require(siteConfigPath)
-
-const patterns = [
-  {
-    from: './assets/data',
-    to: join(OUTPUT_PATH, '/assets/data')
-  },
-  {
-    from: './assets/img',
-    to: join(OUTPUT_PATH, '/assets/img')
-  },
-  {
-    from: './styles/uswds/img',
-    to: join(OUTPUT_PATH, '/uswds/img')
-  },
-  {
-    from: './styles/uswds/js',
-    to: join(OUTPUT_PATH, '/uswds/js')
-  },
-  {
-    from: './assets/fonts/*',
-    to: join(OUTPUT_PATH, '/uswds/fonts/'),
-    flatten: true
-  },
-  {
-    from: './styles/uswds/fonts/**/*',
-    to: join(OUTPUT_PATH, '/uswds/fonts/'),
-    force: true
-  },
-  {
-    from: '**/*',
-    context: uswdsContext,
-    to: join(OUTPUT_PATH, '/uswds/fonts/')
-  },
-  {
-    from: './src/components/federal-agencies/html',
-    to: join(OUTPUT_PATH, '/src/components/federal-agencies/html')
-  },
-  {
-    from: './src/components/about-codedotgov/html',
-    to: join(OUTPUT_PATH, '/src/components/about-codedotgov/html')
-  },
-  {
-    from: './404.html',
-    to: join(OUTPUT_PATH, '404.html')
-  },
-  {
-    from: 'node_modules/@webcomponents/custom-elements/custom-elements.min.js',
-    to: 'polyfills/custom-elements.js'
-  },
-  {
-    from: 'node_modules/custom-event-polyfill/polyfill.js',
-    to: 'polyfills/custom-event.js'
-  },
-  {
-    from: 'node_modules/whatwg-fetch/dist/fetch.umd.js',
-    to: 'polyfills/fetch.js'
-  },
-  {
-    from: 'node_modules/url-search-params-polyfill/index.js',
-    to: 'polyfills/url-search-params.js'
-  },
-  {
-    from: 'node_modules/ajv/dist/ajv.min.js',
-    to: 'external/ajv.min.js'
-  },
-  {
-    from: 'node_modules/jsoneditor/dist',
-    to: 'external/jsoneditor/dist'
-  }
-]
-
-if (process.env.BRANCH && process.env.BRANCH.includes('production')) {
+if (BRANCH.includes('production')) {
   // only include sitemap if building for production on code.gov
   patterns.push({
     from: 'node_modules/@code.gov/site-map-generator/sitemap.xml',
@@ -131,106 +42,55 @@ if (process.env.BRANCH && process.env.BRANCH.includes('production')) {
 }
 
 module.exports = {
-  output: {
-    chunkFilename: '[name].bundle.js',
-    filename: '[name].bundle.js',
-    path: OUTPUT_PATH,
-    publicPath: PUBLIC_PATH
+  entry: {
+    index: ['@babel/polyfill', './src/index.js']
   },
-  entry,
   resolve: {
     modules: ['node_modules', 'src', 'config'],
     extensions: ['.js', '.json', '.md']
   },
-  module: {
-    rules: [
-      {
-        test: /\.(ttf|eot|woff|woff2|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png)$/,
-        loader: 'file-loader',
-        options: {
-          outputPath: 'images'
-        }
-      },
-      {
-        test: /\.(s*)css$/,
-        use: [
-          {
-            loader: 'style-loader', // creates style nodes from JS strings
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'css-loader', // translates CSS into CommonJS
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'sass-loader', // compiles Sass to CSS
-            options: {
-              implementation: sass,
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: [
-              '@babel/plugin-proposal-class-properties',
-              '@babel/plugin-proposal-object-rest-spread',
-              '@babel/plugin-transform-arrow-functions',
-              '@babel/plugin-transform-classes',
-              '@babel/plugin-syntax-dynamic-import',
-              'babel-plugin-dynamic-import-node',
-              'babel-plugin-transform-function-bind',
-              '@babel/plugin-transform-regenerator'
-            ],
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
-      {
-        test: /\.md$/,
-        use: [
-          {
-            loader: 'html-loader'
-          },
-          {
-            loader: 'markdown-loader',
-            options: {
-              /* your options here */
-            }
-          }
-        ]
-      }
-    ]
-  },
   plugins: [
     new DefinePlugin({
-      ENABLE_GOOGLE_ANALYTICS: process.env.CODE_GOV_BRANCH === 'production',
+      ENABLE_GOOGLE_ANALYTICS: BRANCH === 'production',
+      ENABLE_FED_ANALYTICS: BRANCH === 'production',
       PUBLIC_PATH: JSON.stringify(PUBLIC_PATH),
       SITE_CONFIG: JSON.stringify(SITE_CONFIG)
     }),
     new EnvironmentPlugin(['CODE_GOV_API_BASE', 'CODE_GOV_API_KEY', 'CODE_GOV_TASKS_URL']),
-    // new CleanWebpackPlugin([OUTPUT_PATH], { root: rootDir }),
-    new CopyWebpackPlugin({ patterns }),
+    // new MiniCssExtractPlugin({
+    //  filename: "styles/[name].css"
+    // }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'styles', to: 'css' },
+        { from: 'assets/data', to: 'assets/data' },
+        { from: 'assets/img', to: 'assets/img' },
+        {
+          from: './src/components/federal-agencies/html',
+          to: 'src/components/federal-agencies/html'
+        },
+        {
+          from: 'img/**/*',
+          to: 'styles/uswds/',
+          context: path.resolve(rootDir, 'node_modules', 'uswds', 'dist')
+        },
+        {
+          from: 'fonts/**/*',
+          to: 'uswds/',
+          context: path.resolve(rootDir, 'node_modules', 'uswds', 'dist')
+        },
+        {
+          from: 'fonts/*',
+          to: 'uswds/',
+          context: path.resolve(rootDir, 'assets')
+        },
+        {
+          from: 'fonts/*',
+          to: 'assets/',
+          context: path.resolve(rootDir, 'assets')
+        }
+      ]
+    }),
     new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
     new HtmlWebpackPlugin({
       hash: true,
@@ -259,8 +119,164 @@ module.exports = {
         yandex: false
       }
     })
+    // new CleanWebpackPlugin()
   ],
-  watchOptions: {
-    ignored: ['plugins']
+  devServer: {
+    contentBase: path.join(rootDir, 'dist'),
+    compress: true,
+    writeToDisk: true,
+    historyApiFallback: {
+      index: 'index.html'
+    },
+    port: 8080
+  },
+  output: {
+    path: path.resolve(rootDir, 'dist'),
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-transform-arrow-functions',
+              '@babel/plugin-transform-classes',
+              '@babel/plugin-syntax-dynamic-import',
+              'babel-plugin-dynamic-import-node',
+              'babel-plugin-transform-function-bind',
+              '@babel/plugin-transform-regenerator'
+            ],
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      // {
+      //    // Apply rule for .sass, .scss or .css files
+      //  test: /\.css$/,
+
+      //  // Set loaders to transform files.
+      //  // Loaders are applying from right to left(!)
+      //  // The first loader will be applied after others
+      //  use: [
+      //    {
+      //      // After all CSS loaders we use plugin to do his work.
+      //      // It gets all transformed CSS and extracts it into separate
+      //      // single bundled file
+      //      loader: MiniCssExtractPlugin.loader
+      //    },
+      //    'css-loader'
+      //  ]
+      // },
+      // {
+      //    // Apply rule for .sass, .scss or .css files
+      //  test: /\.(sa|sc|c)ss$/,
+
+      //  // Set loaders to transform files.
+      //  // Loaders are applying from right to left(!)
+      //  // The first loader will be applied after others
+      //  use: [
+      //    //{
+      //    //  loader: 'style-loader', // creates style nodes from JS strings
+      //    //  options: {
+      //    //    sourceMap: true
+      //    //  }
+      //    //},
+      //    {
+      //      loader: MiniCssExtractPlugin.loader
+      //    },
+      //    {
+      //      // This loader resolves url() and @imports inside CSS
+      //      loader: "css-loader", options: { url: false, sourceMap: true }
+      //    },
+      //    {
+      //      // Then we apply postCSS fixes like autoprefixer and minifying
+      //      loader: "postcss-loader",
+      //      options: {
+      //        sourceMap: true
+      //      }
+      //    },
+      //    {
+      //      // First we transform SASS to standard CSS
+      //      loader: "sass-loader",
+      //      options: {
+      //        implementation: require("node-sass"),
+      //        sassOptions: {
+      //          sourceMap: true,
+      //          includePaths: [
+      //            //join(dirname(module.filename), 'node_modules'),
+      //            //join(dirname(module.filename), 'node_modules/uswds/dist/scss')
+      //            path.resolve(__dirname, 'node_modules'),
+      //            path.resolve(__dirname, 'node_modules/uswds/dist/scss'),
+      //            path.resolve(__dirname, 'node_modules/uswds/dist/fonts')
+      //          ]
+      //        }
+      //      }
+      //    }
+      //  ]
+      // },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+            options: {
+              implementation: sass,
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.md$/,
+        use: [
+          {
+            loader: 'html-loader'
+          },
+          {
+            loader: 'markdown-loader',
+            options: {
+              /* your options here */
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'images'
+        }
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
   }
 }
