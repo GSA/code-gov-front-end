843cf56f3e629e1b59aa2d3fa2225c9b48dab9aa
Mitchell Henke <mitchell.henke@gsa.gov>
2024-01-11 18:57:40 -0600

    more updates

--- src/build-store.js
+++ src/build-store.js
@@ -1,8 +1,8 @@
 import { applyMiddleware, compose, createStore } from 'redux'
-import { connectRouter, routerMiddleware } from 'connected-react-router'
+import { routerMiddleware } from 'connected-react-router'
 import history from 'browser-history'
 import thunk from 'redux-thunk'
-import rootReducer from './reducers/root'
+import createRootReducer from './reducers/root'
 import { hydrate } from './hydrator'
 
 const initialState = hydrate()
@@ -10,7 +10,7 @@ const initialState = hydrate()
 const buildStore = () => {
   const composeTool = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
   return createStore(
-    connectRouter(history)(rootReducer), // new root reducer with router state
+    createRootReducer(history),
     initialState,
     composeTool(applyMiddleware(routerMiddleware(history), thunk))
   )
--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import { connectRouter } from 'connected-react-router'
 import createParamsReducer from 'utils/reducers/create-params-reducer'
 import createResultsReducer from 'utils/reducers/create-results-reducer'
 import agencies from './agencies'
@@ -12,7 +13,8 @@ import displayMobileMenu from './mobile-menu'
 import expandedMobileMenuOptions from './expanded-mobile-menu-options'
 import searchDropdown from './search-dropdown'
 
-const rootReducer = combineReducers({
+const rootReducer = (history) => combineReducers({
+  router: connectRouter(history),
   agencies,
   agenciesParams: createParamsReducer('AGENCIES'),
   agenciesResults: createResultsReducer('AGENCIES'),
